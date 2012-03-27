99d2d3de705ff14150104bef65b1f86a9fac1db0
rauljin <liujin666jin@gmail.com>
2012-03-27 22:24:00 +0800

    add new

---
+++ MagicBalls/MagicBalls/HealthbarComponent.h
@@ -0,0 +1,18 @@
+//
+//  HealthbarComponent.h
+//  ShootEmUp
+//
+//  Created by Steffen Itterheim on 21.08.10.
+//  Copyright 2010 Steffen Itterheim. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "cocos2d.h"
+
+@interface HealthbarComponent : CCSprite 
+{
+}
+
+-(void) reset;
+
+@end
