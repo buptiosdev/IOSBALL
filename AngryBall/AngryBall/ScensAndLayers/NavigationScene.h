c6ad3bd4b3cbe791a9d5028d52a8650ff7526948
liuyunpeng275 <liuyunpeng275@gmail.com>
2012-04-12 23:09:55 +0800

    lyp move the png

--- AngryBall/AngryBall/ScensAndLayers/NavigationScene.h
+++ AngryBall/AngryBall/ScensAndLayers/NavigationScene.h
@@ -9,7 +9,7 @@
 #import <Foundation/Foundation.h>
 #import "cocos2d.h"
 
-@interface NavigationScene : CCLayer <CCTargetedTouchDelegate>
+@interface NavigationScene : CCLayer
 {
     /*
     CGPoint fingerLocation;
