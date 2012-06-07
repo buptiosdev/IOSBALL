8e535602ae073375e04bed0f034d7c8cb2e69a6e
liuyunpeng275 <liuyunpeng275@gmail.com>
2012-06-07 23:20:53 +0800

    lyp add levelscene and modify loadingscene and pauselayer

---
+++ GreedFamily/GreedFamily/LevelScene.m
@@ -0,0 +1,97 @@
+//
+//  LevelScene.m
+//  GreedFamily
+//
+//  Created by MagicStudio on 12-6-7.
+//  Copyright 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "LevelScene.h"
+#import "LoadingScene.h"
+#import "NavigationScene.h"
+
+@implementation LevelScene
+
+
+-(void)selectMode:(CCMenuItemImage *)btn
+{
+	int mode = btn.tag;
+    [[CCDirector sharedDirector] replaceScene:[LoadingScene sceneWithTargetScene:(TargetScenes)mode]];
+}
+
+-(void)returnMain
+{
+    [[CCDirector sharedDirector] replaceScene:[NavigationScene scene]];
+}
+
+-(id)initWithLevelScene
+{
+    if ((self = [super init])) {
+		self.isTouchEnabled = YES;
+        ccColor4B c = {0,0,0,180};
+        //CCColorLayer * difficulty = [CCColorLayer layerWithColor:c];
+        CCLayerColor * difficulty=[CCLayerColor layerWithColor:c];
+        [self addChild:difficulty z:0 tag:TargetNavigationScen];
+        
+        CCMenuItemImage * easyBtn = [CCMenuItemImage itemFromNormalImage:@"easy.png"
+                                                           selectedImage:@"easy_dwn.png" 
+                                                           disabledImage:@"easy_dis.png"
+                                                                  target:self
+                                                                selector:@selector(selectMode:)];
+        
+        
+        
+        CCMenuItemImage * normalBtn = [CCMenuItemImage itemFromNormalImage:@"normal.png"
+                                                             selectedImage:@"normal_dwn.png" 
+                                                             disabledImage:@"normal_dis.png"
+                                                                    target:self
+                                                                  selector:@selector(selectMode:)];
+        
+        CCMenuItemImage * extremeBtn = [CCMenuItemImage itemFromNormalImage:@"extreme.png"
+                                                              selectedImage:@"extreme_dwn.png" 
+                                                              disabledImage:@"extreme_dis.png"
+                                                                     target:self
+                                                                   selector:@selector(selectMode:)];
+        
+        CCLabelTTF *returnLabel=[CCLabelTTF labelWithString:@"Main Menu" fontName:@"Marker Felt" fontSize:30];
+        [returnLabel setColor:ccRED];
+        CCMenuItemLabel * returnBtn = [CCMenuItemLabel itemWithLabel:returnLabel target:self selector:@selector(returnMain)];
+        
+        [easyBtn setIsEnabled:YES];
+        [normalBtn setIsEnabled:YES];
+        [extremeBtn setIsEnabled:NO];
+        [returnBtn setIsEnabled:YES];
+        
+        [easyBtn setTag:TargetSceneFstScene];
+        [normalBtn setTag:TargetSceneScdScene];
+        [extremeBtn setTag:TargetSceneThrdScene];
+        //[returnBtn setTag:TargetNavigationScen];
+        
+        CCMenu * dMenu = [CCMenu menuWithItems:easyBtn,normalBtn,extremeBtn,returnBtn,nil];
+        [dMenu alignItemsVerticallyWithPadding:10];
+        [difficulty addChild:dMenu];
+    }
+    return self;
+}
+
++(id)scene
+{
+    //order = order;
+	// 'scene' is an autorelease object.
+	CCScene *scene = [CCScene node];
+	
+	// 'layer' is an autorelease object.
+	LevelScene *levelScene = [LevelScene sceneWithLevelScene];
+	
+	// add layer as a child to scene
+	[scene addChild: levelScene];
+    
+	return scene;
+    
+}
+
++(id)sceneWithLevelScene
+{
+    return [[[self alloc] initWithLevelScene] autorelease];
+}
+@end
--- GreedFamily/GreedFamily/PauseLayer.mm
+++ GreedFamily/GreedFamily/PauseLayer.mm
@@ -9,7 +9,7 @@
 #import "PauseLayer.h"
 #import "GameMainScene.h"
 #import "NavigationScene.h"
-#import "LoadingScene.h"
+#import "LevelScene.h"
 
 @implementation PauseLayer
 
@@ -19,49 +19,53 @@
     return [[[PauseLayer alloc] initWithColorLayer:color] autorelease];
 }
 
-- (id) initWithColor1:(ccColor4B)color{
-    if ((self = [super initWithColor:color])) {
-		
-		self.isTouchEnabled=YES;
-		
-		CCSprite * paused = [CCSprite spriteWithFile:@"paused.png"];
-		[paused setPosition:ccp(240,160)];
-		[self addChild:paused];
-		
-    }
-    return self;
-}
+//- (id) initWithColor1:(ccColor4B)color{
+//    if ((self = [super initWithColor:color])) {
+//		
+//		self.isTouchEnabled=YES;
+//		
+//		CCSprite * paused = [CCSprite spriteWithFile:@"paused.png"];
+//		[paused setPosition:ccp(240,160)];
+//		[self addChild:paused];
+//		
+//    }
+//    return self;
+//}
+//
+//
+//
+//- (void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+//{
+//	for( UITouch *touch in touches ) {
+//		CGPoint location = [touch locationInView: [touch view]];
+//		
+//		location = [[CCDirector sharedDirector] convertToGL: location];
+//		
+////		GameLayer * gl = (GameLayer *)[self.parent getChildByTag:KGameLayer];
+////		[gl resume];
+//        [[GameMainScene sharedMainScene]resume];
+//		[self.parent removeChild:self cleanup:YES];
+//	}
+//}
 
-
-
-- (void)ccTouchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+-(void)returnGame
 {
-	for( UITouch *touch in touches ) {
-		CGPoint location = [touch locationInView: [touch view]];
-		
-		location = [[CCDirector sharedDirector] convertToGL: location];
-		
-//		GameLayer * gl = (GameLayer *)[self.parent getChildByTag:KGameLayer];
-//		[gl resume];
-        [[GameMainScene sharedMainScene]resume];
-		[self.parent removeChild:self cleanup:YES];
-	}
-}
-
--(void)continuePlay
-{
-	//start a new game
-    [[GameMainScene sharedMainScene]resume];
+    [[GameMainScene sharedMainScene] resume];
     [self.parent removeChild:self cleanup:YES];
 }
 
 -(void)returnMain
 {
 	//start a new game
-//    [[GameMainScene sharedMainScene]resume];
-//    [self.parent removeChild:self cleanup:YES];
-//    [[CCDirector sharedDirector] runWithScene: [NavigationScene sceneWithNavigationScene]];
-    [[CCDirector sharedDirector] replaceScene:[LoadingScene sceneWithTargetScene:TargetNavigationScen]];
+    [[GameMainScene sharedMainScene] resume];
+    [[CCDirector sharedDirector] replaceScene:[NavigationScene scene]];
+}
+
+-(void)returnLevel
+{
+	//start a new game
+    [[GameMainScene sharedMainScene] resume];
+    [[CCDirector sharedDirector] replaceScene:[LevelScene scene]];
 }
 
 - (id) initWithColorLayer:(ccColor4B)color{
@@ -75,13 +79,17 @@
         
         CCLabelTTF *continueLabel=[CCLabelTTF labelWithString:@"continue" fontName:@"Marker Felt" fontSize:30];
         [continueLabel setColor:ccRED];
-        CCMenuItemLabel * continueBtn = [CCMenuItemLabel itemWithLabel:continueLabel target:self selector:@selector(continuePlay)];
+        CCMenuItemLabel * continueBtn = [CCMenuItemLabel itemWithLabel:continueLabel target:self selector:@selector(returnGame)];
         
         CCLabelTTF *returnLabel=[CCLabelTTF labelWithString:@"Main Menu" fontName:@"Marker Felt" fontSize:30];
         [returnLabel setColor:ccRED];
         CCMenuItemLabel * returnBtn = [CCMenuItemLabel itemWithLabel:returnLabel target:self selector:@selector(returnMain)];
         
-        CCMenu * dMenu = [CCMenu menuWithItems:continueBtn,returnBtn,nil];
+        CCLabelTTF *LevelLabel=[CCLabelTTF labelWithString:@"Level Menu" fontName:@"Marker Felt" fontSize:30];
+        [LevelLabel setColor:ccRED];
+        CCMenuItemLabel * LevelBtn = [CCMenuItemLabel itemWithLabel:LevelLabel target:self selector:@selector(returnLevel)];
+        
+        CCMenu * dMenu = [CCMenu menuWithItems:continueBtn,returnBtn,LevelBtn,nil];
         [dMenu alignItemsVerticallyWithPadding:30];
         [self addChild:dMenu];
     }
