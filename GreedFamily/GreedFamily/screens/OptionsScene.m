76e769eb21c2d856f1bb04152e919862d3228813
liuyunpeng275 <liuyunpeng275@gmail.com>
2012-12-09 09:36:35 +0800

    lyp modify options

--- GreedFamily/GreedFamily/screens/OptionsScene.m
+++ GreedFamily/GreedFamily/screens/OptionsScene.m
@@ -1,8 +1,21 @@
 #import "OptionsScene.h"
 #import "NavigationScene.h"
 #import "SimpleAudioEngine.h"
+#import "CommonLayer.h"
+
+
+//BEGIN item scale  默认为相对于X的比例
+float logolabelscaleY=0.15;
+//END
+
+
 
 @implementation OptionsScene
+
+DeveloperInfo *view;
+//UIActivityIndicatorView *activityIndicatorView;
+//BOOL isCreateIndicatorView;
+
 
 - (id) init {
     self = [super init];
@@ -50,15 +63,6 @@
 		[soundLabel setPosition:ccp((screenSize.width)/3,(screenSize.height)/2)];
 		
 		
-		
-//		CCMenuItemImage * easyBtn = [CCMenuItemImage itemFromNormalImage:@"easy.png"
-//														   selectedImage:@"easy_dwn.png"];
-//		
-//		CCMenuItemImage * normalBtn = [CCMenuItemImage itemFromNormalImage:@"normal.png"
-//															 selectedImage:@"normal_dwn.png" ];
-//		
-//		CCMenuItemToggle * difficulty = [CCMenuItemToggle itemWithTarget:self selector:@selector(changeDifficulty:) items:easyBtn,normalBtn,nil];
-		
 		CCMenuItemImage * unchecked1 = [CCMenuItemImage itemFromNormalImage:@"options_check.png"
 														   selectedImage:@"options_check_d.png"];
 		
@@ -76,11 +80,6 @@
 		CCMenuItemToggle * sound = [CCMenuItemToggle itemWithTarget:self selector:@selector(changeSound:) items:unchecked2,checked2,nil];
 		
 		
-
-//        CCLabelTTF *returnLabel=[CCLabelTTF labelWithString:@"GO Back" fontName:@"Marker Felt" fontSize:25];
-//        [returnLabel setColor:ccRED];
-//        CCMenuItemLabel * returnBtn = [CCMenuItemLabel itemWithLabel:returnLabel target:self selector:@selector(goBack:)];
-//		[difficulty setPosition:ccp(220,350)];
 		[music setPosition:ccp((screenSize.width)*2/3,(screenSize.height)*3/4+10)];
 		[sound setPosition:ccp((screenSize.width)*2/3,(screenSize.height)/2+10)];
 		
@@ -93,10 +92,7 @@
 			sound.selectedIndex = 1;
 		if([usrDef boolForKey:@"music"] == YES)
 			music.selectedIndex = 1;
-//		if([usrDef integerForKey:@"difficulty"] == 0)
-//			difficulty.selectedIndex =0;
-//		else if([usrDef integerForKey:@"difficulty"] == 1)
-//			difficulty.selectedIndex =1;
+
         
         CCSprite *returnBtn = [CCSprite spriteWithSpriteFrameName:@"return.png"];
         CCSprite *returnBtn1 = [CCSprite spriteWithSpriteFrameName:@"return.png"];
@@ -105,26 +101,59 @@
                                                                selectedSprite:returnBtn1 
                                                                        target:self 
                                                                      selector:@selector(returnMain)];
-        returnItem.scale=(45)/[returnBtn contentSize].width; //按照像素定制图片宽高
+        float optscale=(screenSize.height*logolabelscaleY)/[returnBtn contentSize].height;
+        returnItem.scale=optscale; //按照像素定制图片宽高
         CCMenu * returnmenu = [CCMenu menuWithItems:returnItem, nil];
         [returnmenu setPosition:ccp([returnBtn contentSize].width * returnItem.scale * 0.5,
                                     [returnBtn contentSize].height * returnItem.scale * 0.5)];
         
         [self addChild:returnmenu];
+        
+        //set info in the right-down corner
+        CCSprite *info = [CCSprite spriteWithSpriteFrameName:@"unfoldpic.png"];
+        CCSprite *info1 = [CCSprite spriteWithSpriteFrameName:@"unfoldpic.png"];
+        info1.scaleX=1.1;
+        info1.scaleY=1.1;
+        CCMenuItemSprite *infoItem = [CCMenuItemSprite itemFromNormalSprite:info 
+                                                             selectedSprite:info1 
+                                                                     target:self 
+                                                                   selector:@selector(displayInfo:)];
+        infoItem.scale=optscale;
+        CCMenu * infomenu = [CCMenu menuWithItems:infoItem, nil];
+        [infomenu setPosition:ccp(screenSize.width-[info contentSize].width*optscale/2,[info contentSize].height*optscale/2)];
+        [self addChild:infomenu z:1];
+        
+        
 		
     }
     return self;
 }
 
-//-(void)changeDifficulty:(CCMenuItemToggle *)sender
-//{
-//	NSUserDefaults *usrDef = [NSUserDefaults standardUserDefaults];
-//	
-//	if(sender.selectedIndex ==1)
-//		[usrDef setInteger:1 forKey:@"difficulty"];
-//	if(sender.selectedIndex ==0)
-//		[usrDef setInteger:0 forKey:@"difficulty"];
-//}
+//滚动
+-(void)viewAddPointY{
+    view.scrollView.contentOffset = ccpAdd(view.scrollView.contentOffset, ccp(0,0.8));//让UIScrollView显示内容每次慢慢向上移动0.8像素
+    //view.scrollView.contentSize.height :得到UIScrollView的高度
+    if (view.scrollView.contentOffset.y >= view.scrollView.contentSize.height)
+    {
+        view.scrollView.contentOffset = ccp(0,-view.scrollView.frame.size.height+500);
+    }
+}
+
+
+-(void)displayInfo:(id)sender
+{
+    [CommonLayer playAudio:SelectOK];
+    
+//    if (isCreateIndicatorView)
+//    {
+//        [activityIndicatorView stopAnimating ];  //停止  
+//        isCreateIndicatorView = NO;
+//    }
+    view = [[DeveloperInfo alloc] initWithNibName:@"DeveloperInfo" bundle:nil];
+    [[[CCDirector sharedDirector] openGLView] addSubview:view.view];
+    //[view release];
+    [self schedule:@selector(viewAddPointY) interval:0.03];
+}
 
 -(void)changeSound:(CCMenuItemToggle *)sender
 {
